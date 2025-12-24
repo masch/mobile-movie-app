f73160c1b55987a0175fa348fac64b171fbd1a5a
masch <the.masch@gmail.com>
2025-12-24 17:12:37 -0300

    feat: add SearchBar component and update home screen UI with background, logo, and search integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,40 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
+
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import SearchBar from "../components/SearchBar";
 
 export default function Index() {
+  const router = useRouter();
+
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+    <View
+      className="flex-1 bg-primary"
+    >
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+      />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: '100%',
+          paddingBottom: 10
+        }}>
+        <Image
+          source={icons.logo}
+          className="w-12 h-10 mt-20 mb-5 mx-auto"
+        />
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push('/search')}
+            placeholder="Search"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,6 +44,8 @@ export default function Index() {
 
 
 /*
+1:04
+
 // Screen with Styled Components
 import { StyleSheet, Text, View } from "react-native";
 
---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+import { icons } from "@/constants/icons";
+
+interface Props {
+    onPress?: () => void;
+    placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+    return (
+        <View
+            className="flex-row items-center bg-dark-200 rounded-full px-5 py-4"
+        >
+            <Image
+                source={icons.search}
+                className="size-5"
+                resizeMode="contain"
+                tintColor="#ab8bff"
+            />
+            <TextInput
+                onPress={onPress}
+                placeholder={placeholder}
+                value=""
+                onChangeText={(text) => { }}
+                placeholderTextColor="#ab8bdb"
+                className="flex-1 ml-2 text-white"
+            />
+        </View>
+    );
+};
+
+export default SearchBar;
+
