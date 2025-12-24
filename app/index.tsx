d448c15ba6e01d88e13a043889b011570a280a35
masch <the.masch@gmail.com>
2025-12-24 11:45:30 -0300

    feat: add onboarding screen and navigation links to index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,37 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-light-100 font-bold">W</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avengers">Avengers movie</Link>
     </View>
   );
 }
+
+
+
+
+
+/*
+
+import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
+
+const Onboarding = () => {
+    return (
+        <View>
+            <Text>Onboarding</Text>
+        </View>
+    );
+};
+
+
+export default Onboarding;
+const styles = StyleSheet.create({});
+
+
+*/
